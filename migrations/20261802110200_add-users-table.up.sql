0c83db2497111c5e022eb4785df5875a391ab084
Gwenaëlle <gwenaelle.fournier@gmail.com>
2026-02-18 13:00:23 +0100

    Improve migration

--- migrations/20261802110200_add-users-table.up.sql
+++ migrations/20261802110200_add-users-table.up.sql
@@ -2,11 +2,15 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE
+        CHECK (
+            email ~* '^[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$'
+        ),
     password_hash TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW(),
-    deleted_at TIMESTAMPTZ DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
 );
 
 CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_deleted ON users(deleted_at);
